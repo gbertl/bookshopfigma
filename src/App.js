02768af300fa9d0ab7ac94d3db5f11cde58e8526
gilbertlc <gilbert.lacasandile@gmail.com>
2021-04-09 21:47:52 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import user from "./assets/images/user.png";
 import shopbag from "./assets/images/shopbag.png";
 import search from "./assets/images/search.png";
+import banner from "./assets/images/banner.png";
+import arrow from "./assets/images/arrow.png";
 
 function App() {
   return (
@@ -52,6 +54,22 @@ function App() {
           </ul>
         </div>
       </div>
+
+      <section className="header">
+        <div class="wrapper">
+          <img src={banner} alt="" />
+          <div class="promos">
+            <div class="promo-1">
+              <h1>CHANGE OLD BOOK ON NEW</h1>
+              <img src={arrow} alt="" />
+            </div>
+            <div class="promo-2">
+              <h1>TOP 100 BOOKS 2019</h1>
+              <img src={arrow} alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
